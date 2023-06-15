3a8411db9297f1be22ca061d488b5ea54bba2a8d
Adam Terhaerdt <adam.terhaerdt1@gmail.com>
2023-06-15 12:09:46 -0400

    Uploading to app store

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
   final userStream = FirebaseAuth.instance.authStateChanges();
@@ -30,6 +32,26 @@ class AuthService {
         idToken: googleAuth.idToken,
       );
       await FirebaseAuth.instance.signInWithCredential(AuthCredential);
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<bool> get appleSignInAvailable => SignInWithApple.isAvailable();
+
+  Future<void> appleSignIn() async {
+    try {
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+      final oAuthCredential = OAuthProvider('apple.com').credential(
+        idToken: appleCredential.identityToken,
+        accessToken: appleCredential.authorizationCode,
+      );
+      await FirebaseAuth.instance.signInWithCredential(oAuthCredential);
     } on FirebaseAuthException catch (e) {
       print(e);
     }
